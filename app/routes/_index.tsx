934148e551aad63e603e532e7a04aa63e33c2047
Steve Ng <42306483+stnguyenn@users.noreply.github.com>
2024-04-27 02:01:58 -0500

    init blog

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -36,6 +37,11 @@ export default function Index() {
           </a>
         </li>
       </ul>
+      <div className="mx-auto mt-16 max-w-7xl text-center">
+        <Link to="/posts" className="text-xl text-blue-600 underline">
+          Blog Posts
+        </Link>
+      </div>
     </div>
   );
 }
